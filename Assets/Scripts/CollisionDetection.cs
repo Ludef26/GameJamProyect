88d9cf777f7785c23e4ad4b58441fd2806268146
danirodri97 <danird1997@gmail.com>
2023-08-22 01:20:46 +0200

    Colisiones con obstáculos y reinicio del nivel

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            //gameManager.RestartLevel();
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private Scene currentScene;
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
