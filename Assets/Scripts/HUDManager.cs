307ef008da6b00c4c6924d85318600b6e401a1f3
danirodri97 <danird1997@gmail.com>
2023-08-23 18:23:40 +0200

    Tembleque indicador Glide cuando nos quedamos sin

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -8,14 +8,21 @@ public class HUDManager : MonoBehaviour
 {
     public GameObject glideIndicator, dashIndicator;
     private Image glideTimerCircle, dashTimerCircle;
+    private Animator animator;
 
     private void Start()
     {
         glideTimerCircle = glideIndicator.GetComponent<Image>();
+        animator = GetComponent<Animator>();
     }
 
     public void UpdateGlideIndicator(float timerValue, float maxTime)
     {
         glideTimerCircle.fillAmount = timerValue / maxTime;
+
+        if(glideTimerCircle.fillAmount <= 0)
+        {
+            animator.enabled = true;
+        }
     }
 }
