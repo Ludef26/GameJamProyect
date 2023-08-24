f6fe0e3a1ecd0b70157470f4de4e75396ab4e08f
danirodri97 <danird1997@gmail.com>
2023-08-24 14:32:26 +0200

    fix preselección botón en el menu de opciones

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject optionsWindow, backButton, playButton;
+    public EventSystem eventSystem;
+
+    private void Start()
+    {
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+    }
+
+    public void DisplayOptionsWindow()
+    {
+        optionsWindow.SetActive(true);
+        eventSystem.SetSelectedGameObject(backButton);
+    }
+
+    public void HideOptionsWindow()
+    {
+        optionsWindow.SetActive(false);
+        eventSystem.SetSelectedGameObject(playButton);
+    }
 
     public void CargarNivel(string nombreNivel)
     {
