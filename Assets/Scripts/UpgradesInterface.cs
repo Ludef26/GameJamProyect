3f30aca2aca83a5e6a99a4b6cb5e27da6571551c
danirodri97 <danird1997@gmail.com>
2023-08-26 02:46:23 +0200

    Tarjetas progresión visualmente acabadas

--- Assets/Scripts/UpgradesInterface.cs
+++ Assets/Scripts/UpgradesInterface.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UpgradesInterface : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject dashCurrentValueGO, dashNextValueGO, dashCostGO, player;
+    private TMP_Text dashCurrentValue, dashNextValue, dashCost;
+    private PlayerMove playerScript;
+
+    private void Start()
     {
-        
+        player = GameObject.Find("Player");
+        playerScript = player.GetComponent<PlayerMove>();
+
+        dashCurrentValue = dashCurrentValueGO.GetComponent<TextMeshProUGUI>();
+        dashNextValue = dashNextValueGO.GetComponent<TextMeshProUGUI>();
+        dashCost = dashCostGO.GetComponent<TextMeshProUGUI>();
+        dashCost.text = player.GetComponent<PlayerUpgrades>().dashCooldownCost.ToString();
+
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateDashCDCard(float dashCooldownReduction)
     {
-        
+        dashCurrentValue.text = (playerScript.dashCooldown - dashCooldownReduction).ToString();
+        dashNextValue.text = (playerScript.dashCooldown - dashCooldownReduction - dashCooldownReduction).ToString();
+    }
+
+    public void UpdateGlideSecondsCard(float GlideSecondsIncrease)
+    {
+
     }
 }
