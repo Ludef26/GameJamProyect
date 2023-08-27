d9cd70f31f6ece2a8af46e1ab8b3dee3d8533923
danirodri97 <danird1997@gmail.com>
2023-08-27 06:32:15 +0200

    Ventana de victoria

--- Assets/Scripts/FinalTrigger.cs
+++ Assets/Scripts/FinalTrigger.cs
@@ -1,22 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class FinalTrigger : MonoBehaviour
 {
     private GameObject player;
+    public GameObject canvasVictory, hud, points, menuButton;
+    public EventSystem eventSystem;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        eventSystem.SetSelectedGameObject(menuButton);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "ColliderPlayer")
         {
-            Debug.Log("has ganado");
-            // poner el hud de victoria
+            canvasVictory.SetActive(true);
+            hud.SetActive(false);
+            points.SetActive(false);
+            Time.timeScale = 0;
         }
     }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
 }
